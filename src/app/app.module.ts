55141f9a78c4c82e27dd619bd462f197ce05b7b3
Neel <neel@cropin.in>
2017-03-07 19:24:02 +0530

    1st day conclusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { Todo } from '../providers/todo';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -29,6 +30,6 @@ import { ToDoModalPage } from '../pages/to-do-modal/to-do-modal';
     TabsPage,
     ToDoModalPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},{provide :  Todo, useClass :Todo}]
 })
 export class AppModule {}
